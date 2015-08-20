cb0193f4b8260e1f9dbf1d841aa12c02dd98b4c6
dimrirahul <dimrirahul@gmail.com>
2015-08-20 08:07:11 -0700

    blah

--- codeforces/316/D/D.cc
+++ codeforces/316/D/D.cc
@@ -4,141 +4,172 @@
 #define FOR(I,A,B) for(int I = (A); I < (B); ++I)
 #define REP(I,N)   FOR(I,0,N)
 #define ALL(A)     (A).begin(), (A).end()
-
 using namespace std;
 
+#define MP make_pair
+#define PB push_back
 using VI = vector <int>;
-using MAP = unordered_map<int, VI>; 
+using MAP = unordered_map<int, VI>;
 const bool dbg = true;
-const int MD = 22;
+const int MAX_DEPTH = 22;
 const int CHARS = 26;
 
-typedef long long LL;
 const int MX_SZ = (int)5e5 + 10;
 
 struct PI {
-    int first, second, _depth;
-    int _parentByHeight[MD];
-    int _sigma[CHARS];
-
-    PI() {
-        first = second = -1;
-        _depth = 0;
-        memset(_parentByHeight, 0, sizeof(int) * MD);
-        memset(_sigma, 0, sizeof(int) * CHARS);
-    }
-
-    void copyParentByHeight(const PI& other) {
-        for (int i = 0; i < MD; i++) 
-            _parentByHeight[i] = other._parentByHeight[i];
-    }
-
-    void copySigma(const int *sigma) {
-        for (int i = 0; i < CHARS; i++) {
-            _sigma[i] = sigma[i];
-        }
-    }
-
+    int _f, _s, _t; //parent, char, depth
+    PI(int f, int s, int t): _f(f), _s(s), _t(t) {}
+    PI(): _f(0), _s(0), _t(0) {}
     void print() {
-        cout << "Parent = " << first << " Char = " <<  (char) ('a' + second) << 
-            " depth = " << _depth << "\n";
-        for (int i = 0; i < MD; i++) {
-            if (_parentByHeight[i] != 0) {
-                cout << "P at height " << i << " = " << _parentByHeight[i] << "\n";
-            }
-        }
+        printf("PI [parent=%d, char=%c, depth=%d]\n", _f, _s + 'a', _t);
+    }
+};
+struct MD {
+    int _cnt[CHARS];
+    MD() { memset(_cnt, 0, sizeof(int) * CHARS); }
+    void print() {
+        cout << "Chars = [";
+        for (int i = 0; i < CHARS; i++) cout << _cnt[i] << ",";
+        cout << "]\n";
+    }
 
-        for (int i = 0; i < CHARS; i++) {
-            if (_sigma[i] != 0) {
-                cout << "Char = " << (char)('a' + i) << " has count " << _sigma[i] << "\n";
-            }
+};
+struct LI {
+    unordered_map<int, int> _posById;
+    vector<MD> _mdv;
+    void print() {
+        cout << "MAP = [ID, POS] " << "\n";
+        for (auto i : _posById) {
+            cout << "[" << i.first << "," << i.second << "] ";
+        }
+        for (auto& i: _mdv) {
+            i.print();
         }
     }
 };
 
 struct C {
-    VI _map[MX_SZ];
-    PI _pInfo [MX_SZ];
-    VI _lot[MD];
-    VI _travOrder;
+    int _n, _m;
+    VI _lot[MAX_DEPTH];
+    MAP _cById;
+    LI _li[MAX_DEPTH];
+    PI _pi[MX_SZ];
     int _nodeChars[CHARS];
 
-    int _n, _m;
+    void resetNodeChars() { memset(_nodeChars, 0, sizeof(int) * CHARS);}
 
-    bool solve(int nodeId, int depth) {
-        fillNodeChars(nodeId, depth);
-        //if (dbg) printNodeChars();
-        return isPossible();
-    }
 
-    void resetNodeChars() {
-        memset(_nodeChars, 0, sizeof(int) * CHARS);
-    }
-
-    /*
-    void printNodeChars() {
-        for (int i = 0; i < CHARS; i++) {
-            cout << "Char = " << (char) (i + 'a') << " COunt : " << _nodeChars[i] << "\n";
+    void start() {
+        cin >> _n >> _m;
+        _pi[0] = PI(0, -1, 0);
+        _pi[1] = PI(0, 0, 1);
+        REP (i, _n-1) {
+            int t;
+            cin >> t;
+            _pi[i+2]._f = t;
+            _cById[t].push_back(i+2);
         }
-    }
-    */
-
-    size_t findLeftStartPos(int parentNodeId, int depth, bool isRight = false) {
-        VI& nodesAtLevel = _lot[depth];
-        int parentDepth = _pInfo[parentNodeId]._depth;
-        unordered_map <int, int>pLevelNodes;
-        int pos = 0;
-        for (auto &i : _lot[parentDepth]) {
-            pLevelNodes.emplace(i, pos++);
+        REP (i, _n) {
+            char t;
+            cin >> t;
+            _pi[i+1]._s = ((int)t - 'a');
         }
-        int l = 0; int r = nodesAtLevel.size();
-        size_t res = -1;
-        while (l < r) {
-            int m = (l + r) >> 1;
-            // if (dbg) printf("[l=%d, r=%d, m=%d] ", l, r, m);
-            int parentId = _pInfo[nodesAtLevel[m]]._parentByHeight[parentDepth];
+        doBfs();
+        memoize();
+        if (dbg) {
+            for (int i = 1; i <= _n; i++)  _pi[i].print();
+        }
+        for (int i = 0; i < _m; i++) {
+            if ( i > 20) break;
+            cout << " i = " << i <<  "m" << _m << "\n";
+            resetNodeChars();
+            int node, depth;
+            cin >> node >> depth;
+            cout << (solve(node, depth) ? "Yes": "No") << endl;
+        }
+    }    
 
-            if (parentId == parentNodeId) {
-                if (!isRight) {
-                    r = res = m;
-                } else {
-                    l = res = m;
-                    l++;
-                }
-            } else if (pLevelNodes[parentId] < pLevelNodes[parentNodeId]) {
-                l = m + 1;
-            } else {
-                r = m;
+    void doBfs() {
+        using PAIR = pair <int, PI>;
+        queue<PAIR> q;
+        q.push(MP(1, _pi[1]));
+        while (!q.empty()) {
+            PAIR p = q.front();
+            q.pop();
+            _lot[p.second._t].PB(p.first);
+            for (auto &i: _cById[p.first]) {
+                _pi[i]._t = p.second._t + 1;
+                q.push(MP(i, _pi[i]));
             }
+        }
+    }
+
+    void memoize() {
+        int d = 0;
+        for (auto &vi: _lot) {
+            d++;
+            int pos = 0;
+            for (auto &i: vi) {
+                _li[d]._posById.emplace(i, pos++);
+                MD md;
+                vector <MD> &mdv = _li[d]._mdv;
+                if (mdv.size() != 0) md = mdv[mdv.size() - 1];
+                md._cnt[_pi[i]._s]++;
+                mdv.push_back(md);
+            }
+        }
+    }
+
+    int findNode(int v, int d, bool isLeft = true) {
+        int res = v;
+        while (d > 0) {
+            VI& vect = _cById[res];
+            if (isLeft) {
+                res = vect[0];
+            } else {
+                res = vect[vect.size() - 1];
+            }
+            d--;
         }
         return res;
     }
 
-    size_t findRightStartPos(int parentNodeId, int depth) {
-        return findLeftStartPos(parentNodeId, depth, true);
-    }
-
-    void fillNodeChars(int nodeId, int depth) {
-        int heightParent = _pInfo[nodeId]._depth;
-        if (heightParent >= depth) return;
-        size_t pos = findLeftStartPos(nodeId, depth);
-        size_t posR = findRightStartPos(nodeId, depth);
-        VI& nodesAtLevel = _lot[depth];
-
-        PI& leftNode = _pInfo[nodesAtLevel[pos]];
-        PI& rightNode = _pInfo[nodesAtLevel[posR]];
-        for (int i = 0; i < CHARS; i++) {
-            _nodeChars[i] = rightNode._sigma[i] - leftNode._sigma[i];
-            /*
-            if (dbg) {
-                cout << "Left = \n";
-                leftNode.print();
-                cout << "Right = \n";
-                rightNode.print();
-            }
-            */
+    bool solve(int node, int depth) {
+        if (dbg) {
+            cout << "SOlve ------";
+            _pi[node].print();
+            cout << "---------";
         }
-        _nodeChars[leftNode.second]++;
+        int d = depth - _pi[node]._t;
+        if ( d <= 0) return true;
+        int l = findNode(node, d);
+        int r = findNode(node, d, false);
+        LI &lid = _li[depth];
+        MD& leftData = lid._mdv[lid._posById[l]];
+        MD& rightData = lid._mdv[lid._posById[r]];
+        
+        if (dbg) {
+            cout << "LeftNodeId = " << l << " rightnodeId = " << r << 
+                " Left Pos = " << lid._posById[l] <<
+                " Right Pos = " << lid._posById[r] << "\n";
+            cout << " Left MD = \n";
+            leftData.print();
+
+            cout << "Right MD = \n";
+            rightData.print();
+        }
+
+        for (int i = 0; i < CHARS; i++) {
+            _nodeChars[i] = rightData._cnt[i] - leftData._cnt[i];
+        }
+
+        if (dbg) {
+            cout << " Mystery = ";
+            _pi[l].print();
+        }
+
+        _nodeChars[_pi[l]._s]++;
+        return isPossible();
     }
 
     bool isPossible() {
@@ -152,73 +183,6 @@ struct C {
         else return odd == 1;
     }
 
-    void doDfs(int nodeId = 1, int depth = 1, int parentNodeId = 0) {
-        if (parentNodeId != 0)  {
-            _pInfo[nodeId].copyParentByHeight(_pInfo[parentNodeId]);
-            _pInfo[nodeId]._parentByHeight[depth - 1] = parentNodeId;
-        }
-        _pInfo[nodeId]._depth = depth;
-        _lot[depth].push_back(nodeId);
-        //_travOrder.push_back(nodeId);
-        depth++;
-        for (auto& childNode: _map[nodeId]) {
-            doDfs(childNode, depth, nodeId);
-        }
-        //_travOrder.push_back(nodeId);
-    }
-
-    void memoize() {
-        int memz[CHARS] ;
-        for (int i = 0; i < MD; i++) {
-            memset(memz, 0, sizeof(int) * CHARS);
-            for (size_t j = 0; j < _lot[i].size(); j++) {
-                int &nodeId = _lot[i][j];
-                memz[_pInfo[nodeId].second]++;
-                _pInfo[nodeId].copySigma(memz);
-            }
-        }
-    }
-
-    /*
-    void printLot() {
-        for (int i = 0; i < MD; i++) {
-            cout << " Level = " << i << "\n";
-            for (auto &j: _lot[i]) cout << j << " ";
-            cout << "\n";
-        }
-    }
-
-    void printPInfo() {
-        for (int i = 1; i <= 20; i++) {
-            cout << " NodeId = " << i << "\n";
-            _pInfo[i].print();
-        }
-    }
-    */
-    void start() {
-        cin >> _n >> _m;
-        REP (i, _n-1) {
-            int t;
-            cin >> t;
-            _pInfo[i+2].first = t;
-            _map[t].push_back(i+2);
-        }
-        REP (i, _n) {
-            char t;
-            cin >> t;
-            _pInfo[i+1].second = ((int)t - 'a');
-        }
-        doDfs();
-        memoize();
-      //  if (dbg) printLot();
-      //  if (dbg) printPInfo();
-        REP (i, _m) {
-            resetNodeChars();
-            int node, depth;
-            cin >> node >> depth;
-            cout << (solve(node, depth) ? "Yes": "No") << endl;
-        }
-    }    
 };
 
 C t;
